4b6d54234ffd90ede03aa4035e3f775ef2292a98
Jason Bisson <jasonbisson@google.com>
2022-02-24 15:07:50 -0500

    fix linting issue for script

--- files/main.py
+++ files/main.py
@@ -95,132 +95,211 @@ def scc_helper_updated(request):
     # The "sources/-" suffix lists findings across all sources.  You
     # also use a specific source_name instead.
     source_name = "{project_name}/sources/-".format(project_name=project_name)
+    standard = "source_properties.compliance_standards"
+    filter_hcm = "severity=\"HIGH\" OR severity=\"CRITICAL\" \
+        OR severity=\"MEDIUM\""
+    filter_critical = "state=\"ACTIVE\" AND (severity=\"CRITICAL\")"
+    filter_high = "state=\"ACTIVE\" AND (severity=\"HIGH\")"
+    filter_med = "state=\"ACTIVE\" AND (severity=\"MEDIUM\")"
 
     if comp_status is False:
 
-        FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\" OR severity=\"CRITICAL\" OR severity=\"MEDIUM\")"
+        FILTER = "state=\"ACTIVE\" AND ({})".format(filter_hcm)
 
-        finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
 
         # Get Total Findings
         total_findings = finding_result_iterator.total_size
 
         # Get total critical findings
         FILTER = "state=\"ACTIVE\" AND (severity=\"CRITICAL\")"
-        critical_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        critical_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         critical_findings = critical_finding_result_iterator.total_size
         # Get total high findings
         FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\")"
-        high_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        high_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         high_findings = high_finding_result_iterator.total_size
 
         # Get total medium findings
         FILTER = "state=\"ACTIVE\" AND (severity=\"MEDIUM\")"
-        medium_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        medium_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         medium_findings = medium_finding_result_iterator.total_size
     else:
         if compliance == "iso":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\" OR severity=\"CRITICAL\" OR severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"iso\")"
+            FILTER = "state=\"ACTIVE\" AND ({}) AND ({} :\"iso\")".format(
+                filter_hcm, standard)
         elif compliance == "nist":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\" OR severity=\"CRITICAL\" OR severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"nist\")"
+            FILTER = "state=\"ACTIVE\" AND ({}) AND ({} :\"nist\")".format(
+                filter_hcm, standard)
         elif compliance == "cis":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\" OR severity=\"CRITICAL\" OR severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"cis\")"
+            FILTER = "state=\"ACTIVE\" AND ({}) AND ({} :\"cis\")".format(
+                filter_hcm, standard)
         elif compliance == "pci":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\" OR severity=\"CRITICAL\" OR severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"pci\")"
+            FILTER = "state=\"ACTIVE\" AND ({}) AND ({} :\"pci\")".format(
+                filter_hcm, standard)
 
         # Get Total Findings
-        finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         total_findings = finding_result_iterator.total_size
 
         if compliance == "iso":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"CRITICAL\") AND (source_properties.compliance_standards :\"iso\")"
+            FILTER = "{} AND ({} :\"iso\")".format(filter_critical, standard)
         elif compliance == "nist":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"CRITICAL\") AND (source_properties.compliance_standards :\"nist\")"
+            FILTER = "{} AND ({} :\"nist\")".format(filter_critical, standard)
         elif compliance == "cis":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"CRITICAL\") AND (source_properties.compliance_standards :\"cis\")"
+            FILTER = "{} AND ({} :\"cis\")".format(filter_critical, standard)
         elif compliance == "pci":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"CRITICAL\") AND (source_properties.compliance_standards :\"pci\")"
+            FILTER = "{} AND ({} :\"pci\")".format(filter_critical, standard)
 
         # Get total critical findings
-        critical_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        critical_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         critical_findings = critical_finding_result_iterator.total_size
 
         # Get total high findings
         if compliance == "iso":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\") AND (source_properties.compliance_standards :\"iso\")"
+            FILTER = "{} AND ({} :\"iso\")".format(filter_high, standard)
         elif compliance == "nist":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\") AND (source_properties.compliance_standards :\"nist\")"
+            FILTER = "{} AND ({} :\"nist\")".format(filter_high, standard)
         elif compliance == "cis":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\") AND (source_properties.compliance_standards :\"cis\")"
+            FILTER = "{} AND ({} :\"cis\")".format(filter_high, standard)
         elif compliance == "pci":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"HIGH\") AND (source_properties.compliance_standards :\"pci\")"
+            FILTER = "{} AND ({} :\"pci\")".format(filter_high, standard)
 
-        high_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        high_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         high_findings = high_finding_result_iterator.total_size
 
         # Get total Medium findings
         if compliance == "iso":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"iso\")"
+            FILTER = "{} AND ({} :\"iso\")".format(filter_med, standard)
         elif compliance == "nist":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"nist\")"
+            FILTER = "{} AND ({} :\"nist\")".format(filter_med, standard)
         elif compliance == "cis":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"cis\")"
+            FILTER = "{} AND ({} :\"cis\")".format(filter_med, standard)
         elif compliance == "pci":
-            FILTER = "state=\"ACTIVE\" AND (severity=\"MEDIUM\") AND (source_properties.compliance_standards :\"pci\")"
+            FILTER = "{} AND ({} :\"pci\")".format(filter_med, standard)
 
-        medium_finding_result_iterator = client.list_findings(request={"parent": source_name, "filter": FILTER})
+        medium_finding_result_iterator = client.list_findings(
+            request={"parent": source_name, "filter": FILTER})
         medium_findings = medium_finding_result_iterator.total_size
 
     if high_findings > high_max or critical_findings > critical_max or medium_findings > medium_max:
 
         if critical_findings > 0:
-            print('\x1b[6;30;42m' + 'CRITICAL SEVERITY FINDINGS!' + '\x1b[0m' + '\n')
-            for i, finding_result in enumerate(critical_finding_result_iterator):
+            print(
+                '\x1b[6;30;42m' +
+                'CRITICAL SEVERITY FINDINGS!' +
+                '\x1b[0m' +
+                '\n')
+            for i, finding_result in enumerate(
+                    critical_finding_result_iterator):
                 print(
-                    "Name: {}\n Category: {}\n Resource: {}\n URI: {}\n".format(
-                        finding_result.finding.name, finding_result.finding.category, finding_result.finding.resource_name, finding_result.finding.external_uri
-                    )
-                )
-                dict = {"Name": finding_result.finding.name, "Category": finding_result.finding.category, "Resource": finding_result.finding.resource_name, "URI": finding_result.finding.external_uri}
+                    "Name: {}\n Category: {}\n \
+                        Resource: {}\n URI: {}\n".format(
+                        finding_result.finding.name,
+                        finding_result.finding.category,
+                        finding_result.finding.resource_name,
+                        finding_result.finding.external_uri))
+                dict = {
+                    "Name": finding_result.finding.name,
+                    "Category": finding_result.finding.category,
+                    "Resource": finding_result.finding.resource_name,
+                    "URI": finding_result.finding.external_uri}
                 message["key"] = dict
                 write_entry(logger_name, message["key"])
         if high_findings > 0:
-            print('\x1b[6;30;42m' + 'HIGH SEVERITY FINDINGS!' + '\x1b[0m' + '\n')
+            print(
+                '\x1b[6;30;42m' +
+                'HIGH SEVERITY FINDINGS!' +
+                '\x1b[0m' +
+                '\n')
             for i, finding_result in enumerate(high_finding_result_iterator):
                 print(
-                    "Name: {}\n Category: {}\n Resource: {}\n URI: {}\n".format(
-                        finding_result.finding.name, finding_result.finding.category, finding_result.finding.resource_name, finding_result.finding.external_uri
-                    )
-                )
-                dict = {"Name": finding_result.finding.name, "Category": finding_result.finding.category, "Resource": finding_result.finding.resource_name, "URI": finding_result.finding.external_uri}
+                    "Name: {}\n Category: {}\n \
+                        Resource: {}\n URI: {}\n".format(
+                        finding_result.finding.name,
+                        finding_result.finding.category,
+                        finding_result.finding.resource_name,
+                        finding_result.finding.external_uri))
+                dict = {
+                    "Name": finding_result.finding.name,
+                    "Category": finding_result.finding.category,
+                    "Resource": finding_result.finding.resource_name,
+                    "URI": finding_result.finding.external_uri}
                 message["key"] = dict
                 write_entry(logger_name, message["key"])
 
         if medium_findings > 0:
-            print('\x1b[6;30;42m' + 'MEDIUM SEVERITY FINDINGS!' + '\x1b[0m' + '\n')
+            print(
+                '\x1b[6;30;42m' +
+                'MEDIUM SEVERITY FINDINGS!' +
+                '\x1b[0m' +
+                '\n')
             for i, finding_result in enumerate(medium_finding_result_iterator):
                 print(
-                    "Name: {}\n Category: {}\n Resource: {}\n URI: {}\n".format(
-                        finding_result.finding.name, finding_result.finding.category, finding_result.finding.resource_name, finding_result.finding.external_uri
-                    )
-                )
-                dict = {"Name": finding_result.finding.name, "Category": finding_result.finding.category, "Resource": finding_result.finding.resource_name, "URI": finding_result.finding.external_uri}
+                    "Name: {}\n Category: {}\n \
+                        Resource: {}\n URI: {}\n".format(
+                        finding_result.finding.name,
+                        finding_result.finding.category,
+                        finding_result.finding.resource_name,
+                        finding_result.finding.external_uri))
+                dict = {
+                    "Name": finding_result.finding.name,
+                    "Category": finding_result.finding.category,
+                    "Resource": finding_result.finding.resource_name,
+                    "URI": finding_result.finding.external_uri}
                 message["key"] = dict
                 write_entry(logger_name, message["key"])
 
-    print("\x1b[6;30;42m Total SCC Findings: {} Critical Findings: {} High Findings: {} Medium Findings: {} \x1b[0m \n".format(total_findings, critical_findings, high_findings, medium_findings))
+    print(
+        "Total Finding: {} Critical : {} High : {} Medium : {} ".format(
+            total_findings,
+            critical_findings,
+            high_findings,
+            medium_findings))
 
-    if (high_findings > high_max) or (critical_findings > critical_max) or (medium_findings > medium_max):
+    if (high_findings > high_max) or (critical_findings >
+                                      critical_max) or (medium_findings > medium_max):
 
-        print('\x1b[6;30;42m' + 'Build failed! Finding count exceed maximum allowed threshold' + '\x1b[0m')
-        logger.log_text("Build failed! Finding count exceed maximum allowed threshold \n Total SCC Findings: ", severity="ERROR")
-        logger.log_text(str(total_findings) + " Critical Findings: " + str(critical_findings) + " High Findings: " + str(high_findings) + " Medium Findings: " + str(medium_findings), severity="ERROR")
+        print(
+            '\x1b[6;30;42m' +
+            'Build failed! Finding exceed max allowed ' +
+            '\x1b[0m')
+        logger.log_text(
+            "Build failed! Finding exceed max allowed \n Total  Findings: ",
+            severity="ERROR")
+        logger.log_text(
+            str(total_findings) +
+            " Critical Findings: " +
+            str(critical_findings) +
+            " High Findings: " +
+            str(high_findings) +
+            " Medium Findings: " +
+            str(medium_findings),
+            severity="ERROR")
         raise RuntimeError('Build failed!')
 
     else:
-        print('\x1b[6;30;42m' + 'Build passed for threshold provided!' + '\x1b[0m')
+        print(
+            '\x1b[6;30;42m' +
+            'Build passed for threshold provided!' +
+            '\x1b[0m')
         logger.log_text("Build passed or threshold provided!", severity="INFO")
-        logger.log_text(str(total_findings) + " Critical Findings: " + str(critical_findings) + " High Findings: " + str(high_findings) + " Medium Findings: " + str(medium_findings), severity="INFO")
+        logger.log_text(
+            str(total_findings) +
+            " Critical Findings: " +
+            str(critical_findings) +
+            " High Findings: " +
+            str(high_findings) +
+            " Medium Findings: " +
+            str(medium_findings),
+            severity="INFO")
 
 
 def write_entry(logger_name, message):
